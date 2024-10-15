cf8a4ab9918707d846f45fdaf59d1f6fcf3a2503
bruno-pequeno <bru.pequeno@gmail.com>
2024-10-14 23:01:19 -0300

    feat: Creating endpoint for register and save state

--- src/main/java/com/bruno/sistema_vendas/controllers/MainController.java
+++ src/main/java/com/bruno/sistema_vendas/controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("administrative")
+    @GetMapping("administrativo")
     public String mainAcess(){
         return "administrativo/home";
     }
--- src/main/java/com/bruno/sistema_vendas/controllers/StateController.java
+++ src/main/java/com/bruno/sistema_vendas/controllers/StateController.java
@@ -4,7 +4,9 @@ import com.bruno.sistema_vendas.models.StateModel;
 import com.bruno.sistema_vendas.repositories.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,12 +15,20 @@ public class StateController {
     private StateRepository stateRepository;
 
     @GetMapping("/cadastrarEstado")
-    public ModelAndView cadastrar(StateModel stateModel){
+    public ModelAndView cadastrar(StateModel state){
         ModelAndView mv = new ModelAndView("/administrativo/estados/cadastro");
-        mv.addObject("estado", stateModel);
+        mv.addObject("estado", state);
 
         return mv;
     }
 
+    @PostMapping("/salvarEstado")
+    public ModelAndView salvar(StateModel state, BindingResult result){
+        if(result.hasErrors()){
+            return cadastrar(state);
+        }
 
+        stateRepository.saveAndFlush(state);
+        return cadastrar(new StateModel());
+    }
 }
