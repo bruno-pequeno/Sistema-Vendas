3ae7c780adf4fc5f0b65f804c4859b492774d9fc
bruno-pequeno <bru.pequeno@gmail.com>
2024-10-21 23:39:51 -0300

    feat: Editing and removing states and creating listings

--- src/main/java/com/bruno/sistema_vendas/controllers/StateController.java
+++ src/main/java/com/bruno/sistema_vendas/controllers/StateController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class StateController {
@@ -30,5 +33,29 @@ public class StateController {
 
         stateRepository.saveAndFlush(state);
         return cadastrar(new StateModel());
+    }
+
+    @GetMapping("/listarEstado")
+    public ModelAndView listar(){
+        ModelAndView mv = new ModelAndView("administrativo/estados/lista");
+        mv.addObject("listaEstados", stateRepository.findAll());
+
+        return mv;
+    }
+
+    @GetMapping("/editarEstado/{id}")
+    public ModelAndView editar(@PathVariable("id") Long id){
+        Optional<StateModel> state = stateRepository.findById(id);
+
+        return cadastrar(state.get());
+    }
+
+    @GetMapping("/removerEstado/{id}")
+    public ModelAndView remover(@PathVariable("id") Long id){
+        Optional<StateModel> state = stateRepository.findById(id);
+
+        stateRepository.delete(state.get());
+
+        return listar();
     }
 }
